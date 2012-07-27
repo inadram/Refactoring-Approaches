7eca777b6f4b3e9312327fe88a0be6b9e1da27c8
Amir mardani <info@inadram.com>
2012-07-27 14:12:52 +0100

    ExtractInterface
    use of Extract Interface in testing

---
+++ ExtractInterface.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator
+{
+    public class ExtractInterface
+    {
+        private readonly ISomeDetail _someDetail;
+        public ExtractInterface(ISomeDetail someDetail)
+        {
+            _someDetail = someDetail;
+        }
+        public string calculate()
+        {
+            return _someDetail.DoSomeThing();
+        }
+    }
+
+    public interface ISomeDetail
+    {
+        string DoSomeThing();
+    }
+
+    public class SomeDetail : ISomeDetail
+    {
+        public string DoSomeThing()
+        {
+            return "some text";
+        }
+    }
+
+
+
+    [TestFixture]
+    public class testExtractInterface
+    {
+        [Test]
+        public void call_Calculate_should_return()
+        {
+            ISomeDetail fakeSomeDetail = new fakeSomeDetail();
+            var extractInterface = new ExtractInterface(fakeSomeDetail);
+            var value=extractInterface.calculate();
+
+            Assert.That(value, Is.EqualTo("some text"));
+        }
+
+    }
+    public class fakeSomeDetail:ISomeDetail
+    {
+       public string DoSomeThing()
+        {
+            return "some text";
+        }
+    }
+
+
+}
