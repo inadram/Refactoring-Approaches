45e7720bbff73244588ebda02195427c1ead2707
Amir mardani <info@inadram.com>
2012-08-02 14:50:18 +0100

    house keeping

---
+++ ExtractAndOverrideFactoryMethod/AclassWithDependentConstructor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class AclassWithDependentConstructor
+    {
+        private VIPClass vipClass;
+        public AclassWithDependentConstructor()
+        {
+            var ThewirttingIntoFile = new writtingIntoFile();
+            var TheconnectiongToDB = new connectingToDB();
+            vipClass = new VIPClass(ThewirttingIntoFile, TheconnectiongToDB);
+        }
+    }
+
+    public class VIPClass
+    {
+        public VIPClass(writtingIntoFile thewirttingIntoFile, connectingToDB theconnectiongToDb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class connectingToDB
+    {
+        public static void DoSomeTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class writtingIntoFile
+    {
+        public void DoSomeWriting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+   
+}
---
+++ ExtractAndOverrideFactoryMethod/ExtractAndOverrideFactoryMethod.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class ExtractAndOverrideFactoryMethod
+    {
+        private _VIPClass vipClass;
+        public ExtractAndOverrideFactoryMethod()
+        {
+            vipClass = vipclassGetter();
+        }
+
+        protected virtual _VIPClass vipclassGetter()
+        {
+            var ThewirttingIntoFile = new _writtingIntoFile();
+            var TheconnectiongToDB = new _connectingToDB();
+             return vipClass = new _VIPClass(ThewirttingIntoFile, TheconnectiongToDB);
+        }
+    }
+
+    public class _VIPClass
+    {
+        public _VIPClass(_writtingIntoFile thewirttingIntoFile, _connectingToDB theconnectiongToDb)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class _connectingToDB
+    {
+        public static void DoSomeTransaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class _writtingIntoFile
+    {
+        public void DoSomeWriting()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TestExtractAndOverrideFactoryMethod:ExtractAndOverrideFactoryMethod
+    {
+
+        protected override _VIPClass vipclassGetter()
+        {
+            return  fakevipclassGetter();
+        }
+
+        private static _VIPClass fakevipclassGetter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
